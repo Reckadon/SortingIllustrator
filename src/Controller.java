99aa2c902c039d9387660f8b791c9d2513a998e1
Nilay Verma <nilayniarg@gmail.com>
2020-10-07 13:40:04 +0530

    First commit

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,24 @@
 import javafx.scene.chart.BarChart;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 
+import java.util.Random;
+
+
 public class Controller {
-    public Button btnSort;
-    public Slider sizeSlider;
-    public Button btnGenerate;
+    public static Button btnSort;
+    public static Slider sizeSlider;
+    public static Button btnGenerate;
+    public static void arrayGenerate(){
+
+        Random rand =new Random();
+        int arraySize=rand.nextInt(95);
+        int array[]= new int[arraySize];
+        for(int i=0;i<arraySize;i++){
+            array[i]= rand.nextInt(80);
+        }
+    }
     public BarChart BCArray;// BC for Barchart
 }
