0b3f3f71b536cfbce2658509b245d8686cbfa01b
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-10-07 15:13:18 +0530

    Random arrays generation sucessfull

--- src/Controller.java
+++ src/Controller.java
@@ -1,31 +1,32 @@
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 
+import java.util.Arrays;
 import java.util.Random;
 
 
 public class Controller {
     public BarChart BCArray;// BC for Barchart
-    @FXML public static Button btnSort;
-    @FXML public static Slider sizeSlider;
-    @FXML public static Button btnGenerate;
-    public static void arrayGenerate(){
-        sizeSlider.onMouseReleasedProperty();
+    @FXML public Button btnSort;
+    @FXML public Slider sizeSlider;
+    @FXML public Button btnGenerate;
+    
+    @FXML
+    public void arrayGenerate(){
         int sliderValue=(int) sizeSlider.getValue();
         Random rand =new Random();
         int array[]= new int[sliderValue];
         for(int i=0;i<sliderValue;i++){
             array[i]= rand.nextInt(80);
         }
+        System.out.println(Arrays.toString(array));
 
     }
     @FXML
     public void initialize(){
-
+        System.out.println(sizeSlider.getValue()); // timepass(see output-first initialize hota)
     }
 
 }
