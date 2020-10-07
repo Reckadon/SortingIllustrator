595d5ff9a56f2d310c351b8b065529df0e6534bd
Nilay Verma <nilayniarg@gmail.com>
2020-10-07 16:06:30 +0530

    Array sorting with button.

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,12 @@
 import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Arrays;
 
 
 public class Controller {
@@ -12,21 +14,33 @@ public class Controller {
     @FXML public Button btnSort;
     @FXML public Slider sizeSlider;
     @FXML public Button btnGenerate;
-    
-    @FXML
+    int array[];
+    int sliderValue;
     public void arrayGenerate(){
-        int sliderValue=(int) sizeSlider.getValue();
+        sizeSlider.onMouseReleasedProperty();
+        sliderValue=(int) sizeSlider.getValue();
         Random rand =new Random();
-        int array[]= new int[sliderValue];
+        array= new int[sliderValue];
         for(int i=0;i<sliderValue;i++){
             array[i]= rand.nextInt(80);
         }
         System.out.println(Arrays.toString(array));
-
+    }
+    public void arraySort(){
+        for(int i =0;i<sliderValue;i++){
+            for(int j=0;j<(sliderValue-1);j++){
+                if(array[j]>array[j+1]) {
+                    int b=array[j+1];
+                    array[j+1]=array[j];
+                    array[j]=b;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
     @FXML
     public void initialize(){
-        System.out.println(sizeSlider.getValue()); // timepass(see output-first initialize hota)
+        System.out.println(sizeSlider.getValue());
     }
 
 }
