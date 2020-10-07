1055e34fc8e5a7bad89646660eb293177fe1eed8
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-10-07 13:52:34 +0530

    FXML annotations

--- src/Controller.java
+++ src/Controller.java
@@ -1,10 +1,16 @@
+import javafx.fxml.FXML;
 import javafx.scene.chart.BarChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 
 public class Controller {
-    public Button btnSort;
-    public Slider sizeSlider;
-    public Button btnGenerate;
-    public BarChart BCArray;// BC for Barchart
+    @FXML public Button btnSort;
+    @FXML public Slider sizeSlider;
+    @FXML public Button btnGenerate;
+    @FXML public BarChart BCArray;// BC for Barchart
+
+    @FXML
+    public void initialize(){
+
+    }
 }
