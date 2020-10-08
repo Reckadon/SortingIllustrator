6ec4e9e9b14c55ec9695f24316434a9f37eb0c36
Nilay Verma <nilayniarg@gmail.com>
2020-10-08 11:33:36 +0530

    HAHAHAHA

--- src/Controller.java
+++ src/Controller.java
@@ -4,6 +4,8 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 
+import javax.xml.transform.Source;
+import java.util.Arrays;
 import java.util.Random;
 
 
@@ -21,39 +23,50 @@ public class Controller {
         for(int i=0;i<sliderValue;i++){
             array[i]= rand.nextInt(80);
         }
-        arraySort();
+        for(int i=0;i<sliderValue;i++){
+            for(int j=0;j<(sliderValue-1);j++) {
+                if (array[j] == array[j + 1]) {
+                    array[j] = rand.nextInt(80);
+                }
+            }
+        }
+        Thread.sleep(100);
         updateChart(array);
+        System.out.println(Arrays.toString(array));
     }
     public void arraySort() throws InterruptedException {
-        for(int i =0;i<array.length;i++){
-            for(int j=0;j<(array.length-1);j++){
+        for(int i =0;i<sliderValue;i++){
+            for(int j=0;j<(sliderValue-1);j++){
                 if(array[j]>array[j+1]) {
                     int b=array[j+1];
                     array[j+1]=array[j];
                     array[j]=b;
                 }
+                updateChart(array);
+                Thread.sleep(50);
+                System.out.println(Arrays.toString(array));
             }
         }
-        Thread.sleep(1000);
-        updateChart(array);
+
     }
 
     public void updateChart(int[] Tarray) {                    //use this method to put data on the chart
         BCArray.getData().clear();
+        BCArray.layout();
         XYChart.Series series=new XYChart.Series();
-        for (int n:Tarray) {
-            series.getData().add(new XYChart.Data<>(String.valueOf(n),n));
+        for (int n =0;n<Tarray.length;n++) {
+            series.getData().add(new XYChart.Data<>(String.valueOf(Tarray[n]),Tarray[n]));
         }
         series.setName("Numbers");
         BCArray.getData().setAll(series);
-        BCArray.setTitle("Random Array of Size "+Tarray.length+" elements");
+        BCArray.setTitle("Random Array of Size "+sliderValue+" elements");
         BCArray.setLegendVisible(false);
     }
 
+
     @FXML
     public void initialize() throws InterruptedException {
-        arrayGenerate();
-        arraySort();
+         arrayGenerate();
     }
 
 }
