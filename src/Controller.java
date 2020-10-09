c2dadab6b57f9b335b96c023f1ff2bf78c16de74
Romit Mohane <69901221+Reckadon@users.noreply.github.com>
2020-10-09 19:08:12 +0530

    nice hogaya hai

--- src/Controller.java
+++ src/Controller.java
@@ -20,13 +20,13 @@ public class Controller {
     @FXML public Slider sizeSlider;
     @FXML public Button btnGenerate;
     public AnchorPane pane;
+    public Slider speedSlider;
     int array[];
-    int sliderValue;
+    int sliderValue, delayMultiplier =1;
     private int delay=30;
     boolean sorting;
     private Random rand =new Random();
 
-    //TODO duplicates ka kuch karna hai
     public void arrayGenerate() {
         sliderValue=(int) sizeSlider.getValue();
         array= new int[sliderValue];
@@ -34,82 +34,78 @@ public class Controller {
             array[i]= rand.nextInt(496)+5;
         }
         // Sorting array
-        for(int i=0;i<array.length;i++){
-            for(int j=0;j<array.length-1;j++){
-                if(array[j]>array[j+1]){
-                    int t=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=t;
-                }
-            }
-        }
+        Arrays.sort(array);
         // Checking for duplicates
         for(int i=0;i<array.length;i++){
             for(int j=0;j<array.length-1;j++){
-                if(array[j]==array[j+1]){
-                    array[j]=rand.nextInt(496)+5;
+                if(array[j]==array[j+1]){              //if duplicates aside, then increment the number(like 169 169 will become 169 170)
+                    array[j+1]=array[j+1]+1;
                 }
             }
         }
         //Shuffling array
         for(int i=0;i<array.length;i++){
-            for(int j=0;j<array.length-1;j++){
-                int arrayrandomer = rand.nextInt(4);
-                if(arrayrandomer==1 || arrayrandomer==2){
-                    int t=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=t;
+            for(int k=0;k<array.length;k++){
+                for (int j = 0; j <array.length-1 ; j++) {           //3rd for loop cause right side me hi larger numbers aare the on average
+                    if(rand.nextBoolean()){                          //ab nai aare
+                        int t=array[j];
+                        array[j]=array[j+1];
+                        array[j+1]=t;
+                    }
                 }
             }
         }
-       System.out.println(Arrays.toString(array));
         updateChart(array);
-        if(array.length ==0 || array.length <=20) delay=60;
-        else if(array.length <=40) delay = 33;
-        else if(array.length <=60) delay = 15;
-        else delay =10;
+    }
+
+    private void setDelay(){
+        if(array.length ==0 || array.length <=15) delay=40* delayMultiplier;           //delay manager
+        else if(array.length <=30) delay = 18* delayMultiplier;
+        else if(array.length <=40) delay = 13* delayMultiplier;
+        else if(array.length <=60) delay = 10* delayMultiplier;
+        else if(array.length <=75) delay = 8* delayMultiplier;
+        else delay = 5* delayMultiplier;//iske aage mera gpu marjaata lol
     }
 
     private int i=0,j=0,count=0;
     private Node n;
     public void arraySort() {
+        setDelay();
         sorting=true;
         btnSort.setDisable(true);
         btnGenerate.setDisable(true);
         sizeSlider.setDisable(true);
+        speedSlider.setDisable(true);
 
         Timer outerTimer =new Timer();
-        outerTimer.scheduleAtFixedRate(new TimerTask() {
+        outerTimer.scheduleAtFixedRate(new TimerTask() {                       //outer for loop
             @Override
             public void run() {
                 count=0;
                 Timer innerTimer =new Timer();
-                innerTimer.scheduleAtFixedRate(new TimerTask() {
+                innerTimer.scheduleAtFixedRate(new TimerTask() {               //inner for loop
                     @Override
                     public void run() {
-                        if (array[j]>array[j+1]){
+                        if (array[j]>array[j+1]){                              //bubble sort algorithm
                             int t=array[j];
                             array[j]=array[j+1];
                             array[j+1]=t;
                         }else count++;
 
-                        Platform.runLater(new Runnable() {
-                            public void run() {
-                                updateChart(array);
-                            }
-                        });
+                        Platform.runLater(() -> updateChart(array));
+
                         if (count==array.length-1){
                             innerTimer.cancel();
                             outerTimer.cancel();
                             btnSort.setDisable(false);
                             btnGenerate.setDisable(false);
                             sizeSlider.setDisable(false);
+                            speedSlider.setDisable(false);
                             sorting=false;
                             j=0;
                             i=0;
                         }
                         j++;
-                        System.out.println(j);
                         if (j==array.length-1){
                             innerTimer.cancel();
                             j=0;
@@ -118,7 +114,6 @@ public class Controller {
                     }
                 },0,delay);
                 i++;
-                System.out.println(i+"st Timer");
                 if (i==array.length-1){
                     outerTimer.cancel();
                     i=0;
@@ -126,6 +121,7 @@ public class Controller {
                     btnSort.setDisable(false);
                     btnGenerate.setDisable(false);
                     sizeSlider.setDisable(false);
+                    speedSlider.setDisable(false);
                     sorting=false;
                 }
             }
@@ -144,9 +140,8 @@ public class Controller {
         BCArray.getData().setAll(series);
         BCArray.setLegendVisible(true);
 
-        //TODO pure chart ka colour set karna hai with background
         if (sorting){
-            BCArray.setTitle("Bubble sorting Array");
+            BCArray.setTitle("Bubble Sorting Array");                        //nice
             BCArray.lookupAll(".default-color0.chart-bar")
                     .forEach(n -> n.setStyle("-fx-bar-fill: #202020;"));                              //all bars
             BCArray.lookup(".chart-plot-background").setStyle("-fx-background-color: #ccebff;");   //chart bg
@@ -178,7 +173,18 @@ public class Controller {
                     arrayGenerate();
                 }
         );
-        sizeSlider.setShowTickLabels(true); //ye kya kiya hai???
+        sizeSlider.setShowTickLabels(true);               //ye kya kiya hai??? slider pe numbers
+        speedSlider.valueProperty().addListener(
+                (options, oldValue, newValue) ->
+                {
+                    if (newValue.equals(oldValue))
+                        return;
+                    delayMultiplier =newValue.intValue();       //1 to 10
+                    delayMultiplier =11- delayMultiplier;       //inverting
+                    System.out.println(delayMultiplier);
+                    setDelay();
+                }
+        );
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
